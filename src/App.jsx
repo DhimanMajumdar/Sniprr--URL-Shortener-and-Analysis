d00e3bc38649934f84366882f17aafa97fbe527e
DhimanMajumdar <dhimanmajumdar08233@gmail.com>
2025-06-11 21:11:42 +0530

    completed logout functionality

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Link from "./pages/link";
 import RedirectLink from "./pages/redirect-link";
 import Dashboard from "./pages/dashboard";
 import UrlProvider from "./context";
+import RequireAuth from "./components/require-auth";
 
 const router = createBrowserRouter([
   {
@@ -22,7 +23,11 @@ const router = createBrowserRouter([
       },
       {
         path: "/link/:id",
-        element: <Link />,
+        element: (
+          <RequireAuth>
+            <Link />
+          </RequireAuth>
+        ),
       },
       {
         path: "/:id",
@@ -30,7 +35,11 @@ const router = createBrowserRouter([
       },
       {
         path: "/dashboard",
-        element: <Dashboard />,
+        element: (
+          <RequireAuth>
+            <Dashboard />
+          </RequireAuth>
+        ),
       },
     ],
   },
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,63 +1,91 @@
 import { Link, useNavigate } from "react-router";
 import { Button } from "./ui/button.jsx";
+import { UrlState } from "../context.jsx";
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuLabel,
   DropdownMenuTrigger,
-  DropdownMenuSeparator, // import added
+  DropdownMenuSeparator,
 } from "./ui/dropdown-menu.jsx";
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar.jsx";
 import { LinkIcon, LogOut } from "lucide-react";
+import useFetch from "../hooks/use-fetch.jsx";
+import { logout } from "../db/apiAuth.js";
+import { BarLoader } from "react-spinners";
 
 const Header = () => {
   const navigate = useNavigate();
-  const user = false;
+  const { user, fetchUser } = UrlState();
+  const { loading, fn: fnLogout } = useFetch(logout);
 
   return (
-    <header className="w-full bg-transparent py-4 shadow-sm">
-      <div className="max-w-7xl mx-auto px-4 flex justify-between items-center">
-        <Link to="/" className="flex items-center">
-          <img
-            src="/logo.png"
-            className="h-20 md:h-24 object-contain"
-            alt="Sniprr Logo"
-          />
-        </Link>
-        <div>
-          {!user ? (
-            <Button
-              className="cursor-pointer"
-              onClick={() => navigate("/auth")}
-            >
-              Login
-            </Button>
-          ) : (
-            <DropdownMenu>
-              <DropdownMenuTrigger className="rounded-full overflow-hidden">
-                <Avatar>
-                  <AvatarImage src="https://github.com/shadcn.png" />
-                  <AvatarFallback>DM</AvatarFallback>
-                </Avatar>
-              </DropdownMenuTrigger>
-              <DropdownMenuContent>
-                <DropdownMenuLabel>Dhiman Majumdar</DropdownMenuLabel>
-                <DropdownMenuSeparator />
-                <DropdownMenuItem>
-                  <LinkIcon className=" mr-2 h-4 w-4" />
-                  <span>My Links</span>
-                </DropdownMenuItem>
-                <DropdownMenuItem className="text-red-400">
-                  <LogOut className="mr-2 h-4 w-4" />
-                  <span>Logout</span>
-                </DropdownMenuItem>
-              </DropdownMenuContent>
-            </DropdownMenu>
-          )}
+    <>
+      <header className="w-full bg-transparent py-4 shadow-sm">
+        <div className="max-w-7xl mx-auto px-4 flex justify-between items-center">
+          <Link to="/" className="flex items-center">
+            <img
+              src="/logo.png"
+              className="h-20 md:h-24 object-contain"
+              alt="Sniprr Logo"
+            />
+          </Link>
+          <div>
+            {!user ? (
+              <Button
+                className="cursor-pointer"
+                onClick={() => navigate("/auth")}
+              >
+                Login
+              </Button>
+            ) : (
+              <DropdownMenu>
+                <DropdownMenuTrigger className="rounded-full overflow-hidden">
+                  <Avatar>
+                    <AvatarImage
+                      src={user?.user_metadata?.profile_pic}
+                      className="object-contain"
+                    />
+                    <AvatarFallback>
+                      <img
+                        src="https://github.com/shadcn.png"
+                        alt="Image"
+                        className=" border-2 border-blue-400 object-cover hover:scale-105 rounded-4xl"
+                      />
+                    </AvatarFallback>
+                  </Avatar>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent>
+                  <DropdownMenuLabel>
+                    {user?.user_metadata?.name}
+                  </DropdownMenuLabel>
+                  <DropdownMenuSeparator />
+                  <DropdownMenuItem>
+                    <LinkIcon className=" mr-2 h-4 w-4" />
+                    <span>My Links</span>
+                  </DropdownMenuItem>
+                  <DropdownMenuItem className="text-red-400">
+                    <LogOut className="mr-2 h-4 w-4" />
+                    <span
+                      onClick={() => {
+                        fnLogout().then(() => {
+                          fetchUser();
+                          navigate("/");
+                        });
+                      }}
+                    >
+                      Logout
+                    </span>
+                  </DropdownMenuItem>
+                </DropdownMenuContent>
+              </DropdownMenu>
+            )}
+          </div>
         </div>
-      </div>
-    </header>
+      </header>
+      {loading && <BarLoader className="mb-4" width={"100%"} color="#36d7b7" />}
+    </>
   );
 };
 
