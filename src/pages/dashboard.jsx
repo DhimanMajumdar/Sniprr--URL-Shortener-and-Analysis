ac7edb5917acbb7c609e14bd38cf32ab38f079d9
DhimanMajumdar <dhimanmajumdar08233@gmail.com>
2025-06-12 22:46:25 +0530

    done upto dashboard ui

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -1,7 +1,88 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { BarLoader } from "react-spinners";
+import { Button } from "../components/ui/button";
+import { Input } from "../components/ui/input";
+import { UrlState } from "../context";
+import useFetch from "../hooks/use-fetch";
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle,
+} from "../components/ui/card";
+import { Filter } from "lucide-react";
+import { getUrls } from "../db/apiUrls";
+import { getClicksForUrls } from "../db/apiClicks";
+import LinkCard from "../components/link-card";
+//import { Error } from "../components/error";
 
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  const [searchQuery, setSearchQuery] = useState("");
+  const { user } = UrlState();
+  const {
+    loading,
+    error,
+    data: urls,
+    fn: fnUrls,
+  } = useFetch(getUrls, user?.id);
+  const {
+    loading: loadingClicks,
+    data: clicks,
+    fn: fnClicks,
+  } = useFetch(
+    getClicksForUrls,
+    urls?.map((url) => url.id)
+  );
+  useEffect(() => {
+    fnUrls();
+  }, []);
+  useEffect(() => {
+    if (urls?.length) fnClicks();
+  }, [urls?.length]);
+  const filteredUrls = urls?.filter((url) =>
+    url.title.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+  return (
+    <div className=" flex flex-col gap-8">
+      {loading ||
+        (loadingClicks && <BarLoader width={"100%"} color="#36d7b7" />)}
+      <div className="grid grid-cols-2  gap-4">
+        <Card className="border border-blue-400">
+          <CardHeader>
+            <CardTitle>Links Created</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <p>{urls?.length}</p>
+          </CardContent>
+        </Card>
+        <Card className="border border-blue-400">
+          <CardHeader>
+            <CardTitle>Total Clicks</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <p>{clicks?.length}</p>
+          </CardContent>
+        </Card>
+      </div>
+      <div className="flex justify-between">
+        <h1 className=" text-4xl font-extrabold">My Links</h1>
+        <Button>Create Link</Button>
+      </div>
+      <div className="relative border border-blue-400">
+        <Input
+          type="text"
+          placeholder="Filter links.."
+          onChange={(e) => setSearchQuery(e.target.value)}
+          value={searchQuery}
+        />
+        <Filter className=" absolute top-2 right-2 p-1" />
+      </div>
+      {error && <Error message={error?.message} />}
+      {(filteredUrls || []).map((url, id) => {
+        return <LinkCard key={id} url={url} fetchUrls={fnUrls} />;
+      })}
+    </div>
+  );
 };
 
 export default Dashboard;
