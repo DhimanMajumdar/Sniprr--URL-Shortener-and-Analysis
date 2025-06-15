78202e8e6af991ecfd21cbd7870ea278a59d7a57
DhimanMajumdar <dhimanmajumdar08233@gmail.com>
2025-06-15 15:36:19 +0530

    link card changes

--- src/components/link-card.jsx
+++ src/components/link-card.jsx
@@ -45,7 +45,9 @@ const LinkCard = ({ url, fetchUrls }) => {
         <Button
           variant="ghost"
           onClick={() =>
-            navigator.clipboard.writeText(`https://sniprr.in/${url?.short_url}`)
+            navigator.clipboard.writeText(
+              `https://sniprr.netlify.app/${url?.short_url}`
+            )
           }
         >
           <Copy />
